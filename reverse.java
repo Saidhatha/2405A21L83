7a9a375a6a6eac687aeaf9146c56650b5536b388
Saidhatha <matetisaidhatha@gmail.com>
2025-12-14 18:14:48 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,7 @@
+public class reverse {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("Hello ");
+        sb.append("World");
+        System.out.println(sb.reverse());
+    }
+}
