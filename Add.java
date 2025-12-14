8ebd4e26770a5b051dea62fba0b82af969314d59
Saidhatha <matetisaidhatha@gmail.com>
2025-12-14 18:17:51 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        StringBuilder sb=new StringBuilder("M.");
+        sb.append("Sai ");
+        sb.append("Dhatha");
+        System.out.println(sb.append(""));
+    }
+}
